d5afe650bcf2fab2db556e92e5b620b81c0bffd1
unknown <nandiniswaraj13@gmail.com>
2021-03-05 15:26:09 +0530

    [Nandini swaraj]Added Save Wage for Each Company

--- EmployeeWages/Program.cs
+++ EmployeeWages/Program.cs
@@ -3,48 +3,16 @@
 namespace EmployeeWages
 {
     class Program
-    {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-        static void computeEmpWage(String company, int empRatePerHrs, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days:" + totalWorkingDays + "emp Hrs:" + empHrs);
-            }
-
-             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-             Console.WriteLine("Total Emp Wage for Company  " + company +" is : "+totalEmpWage);
-           
-}
+    { 
         public static void Main()
         {
-        computeEmpWage("DMart" ,20,2,10);
-        computeEmpWage("Reliance", 10, 4, 20);
+            EmpWageBuilderObject DMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            DMart.computeEmpWage();
+            EmpWageBuilderObject Relaince = new EmpWageBuilderObject("Relaince", 10, 4, 20);
+            Relaince.computeEmpWage();
 
+
+         
         }
 
     }
