d43a9d3862a13489309f82cd7782ab24c41b4e86
unknown <nandiniswaraj13@gmail.com>
2021-03-05 17:12:36 +0530

    [Nandini swaraj]Added Multiple Company Wage Using InterfaceApproach

---
+++ InterfaceApproach/CompanyEmpWage.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceApproach
+{
+    class CompanyEmpWage
+    {
+        public String company;
+        public int empRatePerHour;
+        public int numOfWorkingDays;
+        public int maxHoursPerMonth;
+        public int totalEmpWage;
+
+        public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
+        {
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHoursPerMonth;
+
+        }
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+          //  Console.WriteLine("called");
+           // Console.WriteLine("value of totalEmpWage" + totalEmpWage);
+
+            this.totalEmpWage = totalEmpWage;
+           // Console.WriteLine("value of totalEmpWage" + totalEmpWage);
+        }
+        public String toString()
+        {
+            return "Tatal Emp Wage For Company " + this.company + " is :" + totalEmpWage;
+        }
+
+    }
+}
---
+++ InterfaceApproach/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InterfaceApproach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+            empWageBuilder.addCompanyEmpWage("DMart",20,2,10);
+            empWageBuilder.addCompanyEmpWage("Relaince",10,4,20);
+            empWageBuilder.computeEmpWage();
+
+
+        }
+    }
+}
